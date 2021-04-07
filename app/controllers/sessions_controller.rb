c99d20bee4b7e6bc8468ab6cb3d8a64668f3e4b1
geraldarzy <geraldarzola@gmail.com>
2021-04-07 00:53:05 -0400

    add create new session/login action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,20 @@
 class SessionsController < ApplicationController
     def create
+        user = User.find_by(email:params['user']['email'])
 
+        if !user
+            #if user is not found
+            render json: {error:'There is no account linked to this email.'}
+
+        elsif !!(user.authenticate(params['user']['password']))
+            #if pw matches pw confirmation
+            session[:id] = user.id
+            session[:email] = user.email
+            puts params
+            render json: {session:session }
+        else
+            render json: {error:'Password incorrect, please try again.'}
+        end
     end
 
     def destroy
