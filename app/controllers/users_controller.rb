8b254271f973a499662c19166d48548ff32dbc3b
geraldarzy <geraldarzola@gmail.com>
2021-04-06 21:57:18 -0400

    log in user after signed up(store info in session)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:id] = @user.id
+      session[:email] = @user.email
       render json: @user, status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
